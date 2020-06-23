ed1358f9855a40b14c744c59a666c4fa7f37d85b
yuzhanglong <yuzl1123@163.com>
2020-06-24 00:25:27 +0800

    项目初始化

---
+++ src/main/java/com/yzl/judgehost/JudgeHostApplication.java
@@ -0,0 +1,16 @@
+package com.yzl.judgehost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuzhanglong
+ */
+@SpringBootApplication
+public class JudgeHostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JudgeHostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzl/judgehost/JudgeHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzl.judgehost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeHostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
