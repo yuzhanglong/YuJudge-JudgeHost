95d42331b5d619d7dabfcb0586652a61b67ed013
yuzhanglong <yuzl1123@163.com>
2020-09-04 15:33:48 +0800

    feat: 使用docker-compose管理项目

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
+
 # 构建编译所需要的文件
+
 cd ..
 
 rm -rf build
@@ -6,5 +9,7 @@ rm -rf build
 mkdir build
 
 cp -r src/main/java/com/yzl/judgehost/scripts build
-cp target/YuJudge-JudgeHost-0.0.1-SNAPSHOT.jar build/app.jar
+cp target/YuJudge-JudgeHost-1.0.jar build/app.jar
 cp deploy/Dockerfile build
+cp deploy/docker-compose.yml build
+cp deploy/entrypoint.sh build
---
+++ deploy/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+java -jar app.jar \
+          --spring.redis.port=$REDIS_PORT \
+          --spring.redis.host=$REDIS_HOST \
+          --judge-authorization.user-id=$JUDGE_HOST_USER_ID \
+          --judge-authorization.user-secret=$JUDGE_HOST_USER_SECRET \
+          --judge-authorization.user-id=$JUDGE_HOST_SECRET_KEY
