600f898c6bca3b14d8e60b7709281dce3da9c737
yizhi <2718994735@qq.com>
2023-05-25 12:08:25 +0800

    fix:中文代码乱码

--- src/main/java/com/yzl/judgehost/service/JudgeService.java
+++ src/main/java/com/yzl/judgehost/service/JudgeService.java
@@ -84,7 +84,7 @@ public class JudgeService {
 
         // 执行编译脚本
         try {
-            String[] envp = {"LANG=UTF-8"};
+            String[] envp = {"LANG=GBK"};
             Process process = JudgeHolder.getRunner().exec(
                     new String[]{
                             compileScript,
