9b3921d8c6df1861af84d9437c6a21c7dafaaa8b
yizhi <2718994735@qq.com>
2023-05-26 13:58:15 +0800

    fix:

--- src/main/java/com/yzl/judgehost/service/JudgeService.java
+++ src/main/java/com/yzl/judgehost/service/JudgeService.java
@@ -84,7 +84,7 @@ public class JudgeService {
 
         // 执行编译脚本
         try {
-            String[] envp = {"LANG=GBK"};
+            String[] envp = {"LANG=UTF-8"};
             Process process = JudgeHolder.getRunner().exec(
                     new String[]{
                             compileScript,
